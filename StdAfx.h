40c9f52f0ccbd5f188e3620da4fd92de8994eb69
陈粤 <cy@cgkc12366.com>
2025-07-27 10:39:25 +0800

    清理垃圾

--- StdAfx.h
+++ StdAfx.h
@@ -15,7 +15,7 @@
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <windows.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 
 // TODO: reference additional headers your program requires here
